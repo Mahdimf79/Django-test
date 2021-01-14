ef834d3f5dfad7bfebbd349f0be455762a4a4807
Mohammad Mahdi Mollaee <mahdi32135@gmail.com>
2021-01-14 21:46:25 +0330

    initial commit

---
+++ firstApp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Question(models.Model):
+    text=       models.CharField(max_length=300)
+    pub_date=   models.DateTimeField()
+
+    def __str__(self):
+        return self.text
+
+class Choice(models.Model):
+    question=   models.ForeignKey(Question, on_delete=models.CASCADE)
+    text=       models.CharField(max_length=300)
+    vote=       models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
