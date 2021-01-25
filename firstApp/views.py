7deb4b09d03b900ff598dfb08e3160dc7dd30473
Mohammad Mahdi Mollaee <mahdi32135@gmail.com>
2021-01-25 03:49:30 +0330

    fix bog

--- firstApp/views.py
+++ firstApp/views.py
@@ -7,6 +7,7 @@ from django_globals import globals
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.csrf import csrf_exempt
+import re
 
 
 def index(request):
@@ -103,27 +104,75 @@ def register_set(request):
     lastname = request.POST['lastname']
     password = request.POST['password']
     repassword = request.POST['repassword']
-    status = False
+    status = True
 
-    if password == repassword:
-        try:
-            usercheck = models.Username.objects.get(username = username)
+    if len(password) >= 8 and len(password) <= 64:
+        if len(username) >= 5 and len(username) <= 30:
+            if password == repassword:
+                if re.fullmatch('(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))',email):
+                    if re.fullmatch('[A-Za-z]{2,25}( [A-Za-z]{2,30})?', firstname):
+                        if re.fullmatch('[A-Za-z]{2,25}( [A-Za-z]{2,30})?', lastname):
+
+                            try:
+                                print('khar')
+                                usercheck = models.Username.objects.get(username = username)
+                                print('khar2')
+                                obj = {
+                                    'ok': False,
+                                    'status' : 'This username is available'
+                                }
+                                status = False
+                                print('khar3')
+                            except:
+                                pass
+
+                            try:
+                                emailcheck = models.Username.objects.get(email = email)
+                                obj = {
+                                    'ok': False,
+                                    'status' : 'This email is available'
+                                }
+                                status = False
+                            except:
+                                pass
+
+                            if status:
+                                user = models.Username(username = username, email = email, firstname = firstname,
+                                lastname = lastname, password = password )
+                                user.save()
+                                obj = {
+                                'ok': True,
+                                'status' : 'sucssecful'
+                                }
+                        else:
+                            obj = {
+                                'ok': False,
+                                'status' : 'Invalid Lastname'
+                            }
+                    else:
+                        obj = {
+                            'ok': False,
+                            'status' : 'Invalid Firstname'
+                        }
+                else:
+                    obj = {
+                        'ok': False,
+                        'status' : 'Invalid Email'
+                    }
+            else:
+                obj = {
+                    'ok': False,
+                    'status' : 'A password is not the same as repeating a passworde'
+                }
+        else:
             obj = {
                 'ok': False,
-                'status' : 'This username is available'
-            }
-        except:
-            user = models.Username(username = username, email = email, firstname = firstname,
-                lastname = lastname, password = password )
-            user.save()
-            obj = {
-                'ok': True,
-                'status' : 'sucssecful'
+                'status' : 'The number of characters in the username is not allowed'
             }
     else:
         obj = {
             'ok': False,
-            'status' : 'A password is not the same as repeating a passworde'
+            'status' : 'The number of characters in the password is not allowed'
         }
     return JsonResponse(obj)
 
